853e4cb91cad269c0899258567af2eda19fade05
Paul Hsieh <jeinhone.hsieh@gmail.com>
2020-12-15 15:43:50 +0800

    optimized CChar solution

--- 680. Valid Palindrome II/ValidPalindromeII_CChar.swift
+++ 680. Valid Palindrome II/ValidPalindromeII_CChar.swift
@@ -5,8 +5,8 @@
 //  Created by Paul Hsieh on 2020/12/15.
 //
 
-/// Runtime: 72 ms, faster than 93.47% of Swift online submissions for Valid Palindrome II.
-/// Memory Usage: 17.6 MB, less than 5.71% of Swift online submissions for Valid Palindrome II.
+/// Runtime: 68 ms, faster than 94.69% of Swift online submissions for Valid Palindrome II.
+/// Memory Usage: 16.9 MB, less than 6.53% of Swift online submissions for Valid Palindrome II.
 
 class Solution {
     func validPalindrome(_ s: String) -> Bool {
@@ -22,18 +22,14 @@ class Solution {
         if startIndex >= endIndex {
             return true
         } else {
-            var leftArray = charArray
-            leftArray.remove(at: startIndex)
-            var rightArray = charArray
-            rightArray.remove(at: endIndex)
-            return validate(leftArray, from: 0, to: leftArray.count - 2) || validate(rightArray, from: 0, to: rightArray.count - 2)
+            return validate(charArray, from: startIndex + 1, to: endIndex) || validate(charArray, from: startIndex, to: endIndex - 1)
         }
     }
     
     func validate(_ s: [CChar], from: Int, to: Int) -> Bool {
         guard s.count > 2 else { return true }
-        var startIndex = 0
-        var endIndex = s.count - 2
+        var startIndex = from
+        var endIndex = to
         while startIndex < to {
             if s[startIndex] != s[endIndex] {
                 return false
